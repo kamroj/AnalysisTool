113d046d04a1a369426ef7b4de373a8666b2bdd3
student2 <student2@pcoip.ki.agh.edu.pl>
2019-06-01 12:56:39 +0200

    up

--- src/main/java/com/koguty/spocone/Main.java
+++ src/main/java/com/koguty/spocone/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
+		System.out.println("*********************");
+		System.out.println("***   Welcome in  ***");
 		System.out.println("*** Analysis Tool ***");
-		System.out.println("Wpisz scieżkę");
+		System.out.println("*********************");
+		System.out.println("Podaj ścieżkę do danych: ");
 		Scanner sc = new Scanner(System.in);
 		String path = sc.nextLine();
-		System.out.println("Wybierz opcję\n");
-		System.out.println("1 - raport 1\n" + "2 - raport 2\n" + "v - wersja\n" + "h - help");
+		System.out.println("Wybierz opcję:\n");
+		System.out.println("1 - raport 1\n" + "2 - raport 2\n" + "v - wersja\n" + "h - help\n"+ "e - exit");
 		String choice = sc.nextLine();
+			
 		switch (choice) {
 		case "1": {
 //        	printRaport1();
@@ -20,23 +24,15 @@ class Main {
 //        	printRaport2();
 			break;
 		}
-		case "3": {
-//        	printRaport3();
-			break;
-		}
-		case "4": {
-//        	printRaport4();
-			break;
-		}
-		case "5": {
-//        	printRaport5();
-			break;
-		}
 		case "h": {
 //        	printHelp();
 			break;
 		}
 		case "v": {
+//        	printVersion();
+			break;
+		}
+		case "e": {
 //        	printVersion();
 			break;
 		}
