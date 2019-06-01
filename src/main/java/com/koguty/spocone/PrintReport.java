458c030220b3b45423aeafe026d87c46e433fea2
student5 <student5@pcoip.ki.agh.edu.pl>
2019-06-01 13:52:17 +0200

    2

--- src/main/java/com/koguty/spocone/PrintReport.java
+++ src/main/java/com/koguty/spocone/PrintReport.java
@@ -1,7 +1,22 @@
 package com.koguty.spocone;
 
-public class PrintReport {
-	private PrintReport printReport = new PrintReport();
+import java.util.ArrayList;
 
-	
+public class PrintReport {
+
+	void printEmployeesReport(ArrayList<Employee> employees) {
+		System.out.println("Raport liczby godzin przepracowawanych przez poszczególnych pracowników");
+
+		for (Employee employee : employees) {
+			System.out.println(employee.getPersonalDetails() + " " + employee.getWorkingData());
+		}
+	}
+
+	void printProjectsReport(ArrayList<Project> projects) {
+		System.out.println("Raport liczby godzin poświęconych na poszczególne projekty");
+
+		for (Project project : projects) {
+			System.out.println(project.getName() + " " + project.getWorkingData());
+		}
+	}
 }
