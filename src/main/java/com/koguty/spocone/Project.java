8d2c5d8eef8273e1a6f6aa48c1982337dead0ac6
Kamil <kamroj@gmail.com>
2019-06-01 12:40:56 +0200

    Create Employee, Project classes

---
+++ src/main/java/com/koguty/spocone/Project.java
@@ -0,0 +1,13 @@
+package com.koguty.spocone;
+
+class Project {
+    private String name;
+
+    Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
