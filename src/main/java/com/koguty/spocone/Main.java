39d31a65cab45ee8df675cbdcae28e0f192a0c78
student1 <student1@pcoip.ki.agh.edu.pl>
2019-06-01 12:40:47 +0200

    inital commit

--- src/main/java/com/koguty/spocone/Main.java
+++ src/main/java/com/koguty/spocone/Main.java
@@ -1,7 +1,47 @@
 package com.koguty.spocone;
 
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
+	public static void main(String[] args) {
+		System.out.println("*** Analysis Tool ***");
+		System.out.println("Wpisz scieżkę");
+		Scanner sc = new Scanner(System.in);
+		String path = sc.nextLine();
+		System.out.println("Wybierz opcję\n");
+		System.out.println("1 - raport 1\n" + "2 - raport 2\n" + "v - wersja\n" + "h - help");
+		String choice = sc.nextLine();
+		switch (choice) {
+		case "1": {
+//        	printRaport1();
+			break;
+		}
+		case "2": {
+//        	printRaport2();
+			break;
+		}
+		case "3": {
+//        	printRaport3();
+			break;
+		}
+		case "4": {
+//        	printRaport4();
+			break;
+		}
+		case "5": {
+//        	printRaport5();
+			break;
+		}
+		case "h": {
+//        	printHelp();
+			break;
+		}
+		case "v": {
+//        	printVersion();
+			break;
+		}
+
+		}
+
+	}
 }
